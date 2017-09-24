6b081461dadafe8a46d610f0875304c841cff92e
Faugermire <faugermire@gmail.com>
2017-09-23 19:12:24 -0600

    Verify Inputs added

--- WilleHinzley.py
+++ WilleHinzley.py
@@ -1,3 +1,24 @@
+import sys
+import json
+
+def write_json(obj, filename):
+    with open(filename, mode='w') as f:
+        json.dump(obj, f)
+
+def read_json(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+def inputVerify():
+    files = []
+    if len(sys.argv) >= 2:
+        files.append(sys.argv[1])
+        files.append(sys.argv[2])
+        return files
+    else:
+        print('Please, input both an input and output filename.')
+        exit(-1)
+
 if __name__ == '__main__':
-    print('The beginning of something great.')
-    print('Does Github like this or nah')
+    files = inputVerify()
+    print(files[0] + " " + files[1])
